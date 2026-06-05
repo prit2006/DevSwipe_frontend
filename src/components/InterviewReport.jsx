ba1624dda42eb89d53fd8949ccab2d44c9d66bd4
prit2006 <pritpastagiya2006@gmail.com>
2026-06-05 18:07:11 +0530

    update resume download filename to include 'Improved' prefix

--- src/components/InterviewReport.jsx
+++ src/components/InterviewReport.jsx
@@ -43,7 +43,7 @@ const InterviewReport = () => {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
-      link.setAttribute('download', `AI_Resume_${id}.pdf`);
+      link.setAttribute('download', `Improved_AI_Resume_${id}.pdf`);
       document.body.appendChild(link);
       link.click();
       
